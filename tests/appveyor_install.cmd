5d246144e42e945b0c11ad814ab2c2e40b45c99f
Mikhail Denisenko <denisenkom@gmail.com>
2018-03-19 21:28:48 -0400

    display swig version

--- tests/appveyor_install.cmd
+++ tests/appveyor_install.cmd
@@ -10,6 +10,7 @@ REM Only install swig if it isn't present (as a result of AppVeyor's caching).
 REM SWIG 2.0.11 is the minimum required version, but it does not yet exist in
 REM Chocolatey.
 IF NOT EXIST C:\ProgramData\chocolatey\bin\swig.exe choco install swig --version 2.0.12 --allow-empty-checksums --yes --limit-output || goto :error
+swig -version || goto :error
 
 curl -Lo softhsm.zip https://github.com/disig/SoftHSM2-for-Windows/releases/download/v2.3.0/SoftHSM2-2.3.0-portable.zip || goto :error
 7z -bb3 -oc:\\ x softhsm.zip || goto :error
