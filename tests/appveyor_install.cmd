5f36e9e6bf9cf0aa1456e11fcd589f807237ff8c
denisenk <denisenk@amazon.com>
2018-03-19 21:01:38 -0400

    fixup

--- tests/appveyor_install.cmd
+++ tests/appveyor_install.cmd
@@ -9,7 +9,7 @@ REM Use Chocolatey to install SWIG.
 REM Only install swig if it isn't present (as a result of AppVeyor's caching).
 REM SWIG 2.0.11 is the minimum required version, but it does not yet exist in
 REM Chocolatey.
-IF NOT EXIST C:\ProgramData\chocolatey\bin\swig.exe choco install swig --version 2.0.11 --allow-empty-checksums --yes --limit-output #> $null
+IF NOT EXIST C:\ProgramData\chocolatey\bin\swig.exe choco install swig --version 2.0.11 --allow-empty-checksums --yes --limit-output
 
 curl -Lo softhsm.zip https://github.com/disig/SoftHSM2-for-Windows/releases/download/v2.3.0/SoftHSM2-2.3.0-portable.zip
 7z -bb3 -oc:\\ x softhsm.zip
